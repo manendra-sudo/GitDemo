d7a24047e501684b5ba77fe7bc4562173ce75cf6
manendra <kumar.addepalli1@gmail.com>
2020-08-05 13:36:46 -0400

    first commit

---
+++ src/test/java/MavenAcademy/MavenTest/AppiumTest.java
@@ -0,0 +1,21 @@
+package MavenAcademy.MavenTest;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	
+	@Test
+	public void NativeAndroidApp()
+	{
+		System.out.println("NativeAndroidApp");
+	}
+	
+	@Test
+	public void IOSapp()
+	{
+		System.out.println("IOSapp");
+	}
+
+
+}
